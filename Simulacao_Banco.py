7dd52d77a8ce26a7a3c0cf624b27971c09886093
Marii Martins <marii.meto@gmail.com>
2023-07-24 14:44:18 -0300

    Atualização

---
+++ Simulacao_Banco.py
@@ -0,0 +1,70 @@
+def main():
+    # Pergunta se o usuário já tem conta
+    has_account = input("Você já tem uma conta bancária? (S/N): ").upper()
+
+    if has_account == "S":
+        saldo = 1000  # Saldo inicial para quem já possui conta
+    elif has_account == "N":
+        saldo = float(input("Digite o valor inicial para abrir a conta: "))
+    else:
+        print("Opção inválida. Encerrando o programa.")
+        return
+
+    while True:
+        print("\nEscolha uma opção:")
+        print("1 - Verificar saldo")
+        print("2 - Fazer depósito")
+        print("3 - Fazer saque")
+        print("4 - Solicitar empréstimo/financiamento")
+        print("0 - Encerrar o programa")
+
+        opcao = int(input("Opção: "))
+
+        if opcao == 0:
+            print("Encerrando o programa.")
+            break
+        elif opcao == 1:
+            print(f"Seu saldo atual é R$ {saldo:.2f}")
+        elif opcao == 2:
+            deposito = float(input("Digite o valor do depósito: "))
+            saldo += deposito
+            print(f"Depósito de R$ {deposito:.2f} realizado. Seu saldo atual é R$ {saldo:.2f}")
+        elif opcao == 3:
+            saque = float(input("Digite o valor do saque: "))
+            if saque <= saldo:
+                saldo -= saque
+                print(f"Saque de R$ {saque:.2f} realizado. Seu saldo atual é R$ {saldo:.2f}")
+            else:
+                print("Saldo insuficiente. Operação não realizada.")
+        elif opcao == 4:
+            salario = float(input("Digite sua renda: "))
+            valor_liberado = salario * 35 / 100 * 12
+
+            valor_solicitado = float(input("Digite o valor do empréstimo desejado: "))
+            if valor_solicitado > valor_liberado:
+                print(f"Valor solicitado maior que o valor máximo disponibilizado. O valor disponibilizado é de R$ {valor_liberado:.2f}")
+            else:
+                parcelas = int(input("Digite em quantas parcelas deseja pagar (1 até 12): "))
+                while parcelas > 12 or parcelas == 0:
+                    print("Número de parcelas deve ser entre 1 e 12.")
+                    parcelas = int(input("Digite em quantas parcelas deseja pagar: "))
+
+                if parcelas == 1:
+                    taxa_juros = 0
+                elif 2 <= parcelas <= 5:
+                    taxa_juros = 3.5
+                elif 6 <= parcelas <= 8:
+                    taxa_juros = 5
+                else:
+                    taxa_juros = 9
+
+                valor_final = valor_solicitado * (1 + taxa_juros / 100) / parcelas
+
+                print(f"Taxa de Juros: {taxa_juros:.2f} %")
+                print(f"{parcelas} parcelas mensais de: R$ {valor_final:.2f}")
+        else:
+            print("Opção inválida. Tente novamente.")
+
+
+if __name__ == "__main__":
+    main()
